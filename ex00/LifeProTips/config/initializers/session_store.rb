be59c1e495c386db0c15eab712504dba0d53de02
Patrice Cadiot <pathibul.r@gmail.com>
2019-10-01 13:18:12 +0200

    reformate sessions_store pour ne pas avoir une ligne trop longue

--- ex00/LifeProTips/config/initializers/session_store.rb
+++ ex00/LifeProTips/config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_LifeProTips_session', expires_after: 60.seconds
+Rails.application.config.session_store :cookie_store,
+                                       key: '_LifeProTips_session',
+                                       expires_after: 60.seconds
