78d64e753c376bef0e5abe5c8c9731adaf38ff5e
Patrice Cadiot <quarkgluant@users.noreply.github.com>
2019-11-26 14:40:37 +0100

    Update application_controller.rb
    transforme le find en find_by, car find_by retourne nil s'il ne trouve pas, alors que find lève une exception

--- ex02/LifeProTips/app/controllers/application_controller.rb
+++ ex02/LifeProTips/app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :admin?, :get_random_animal, :authenticate_user
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
   def admin?
